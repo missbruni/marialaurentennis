96f319e0e1984d09317155bd9586e78ad30559f5
bruna <brunalima@me.com>
2025-07-22 00:39:43 +0100

    improve react query config

--- src/lib/react-query.tsx
+++ src/lib/react-query.tsx
@@ -1,9 +1,21 @@
-"use client";
+'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const FIVE_MINUTES = 5 * 60 * 1000;
+const TEN_MINUTES = 10 * 60 * 1000;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: FIVE_MINUTES,
+      gcTime: TEN_MINUTES,
+      retry: 1,
+      refetchOnWindowFocus: false
+    }
+  }
+});
 type ReactQueryProviderProps = {
   children: React.ReactNode;
 };
